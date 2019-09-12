665ef871f3e2ea2a47099c6f9c67c598f311ced3
Genta Ito <itogenta.toge@gmail.com>
2019-09-12 22:55:18 +0900

    リセットボタン追加

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
 
-export default class TodoApp extends React.Component {
+interface State {
+  tasks: { title: string; id: number }[];
+  uniqueId: number;
+}
+export default class TodoApp extends React.Component<{}, State> {
   state = {
     tasks: [{ title: "Todo１つ目", id: 0 }, { title: "Todo２つ目", id: 1 }],
     uniqueId: 1
@@ -12,11 +16,17 @@ export default class TodoApp extends React.Component {
     tasks.push({ title, id: uniqueId });
     this.setState({ tasks, uniqueId: uniqueId + 1 });
   };
+  resetTodo = () => {
+    this.setState({
+      tasks: []
+    });
+  };
 
   render() {
     return (
       <div>
         <h1>TODO App</h1>
+        <button onClick={this.resetTodo}>リセット</button>
         <TodoInput addTodo={this.addTodo} />
         <TodoList tasks={this.state.tasks} />
       </div>
